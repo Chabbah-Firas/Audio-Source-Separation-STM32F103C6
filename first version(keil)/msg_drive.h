874d273cd08e6786c70da9be70cf015835467d63
Chabbah <120501586+Chabbah-Firas@users.noreply.github.com>
2023-11-12 20:39:59 +0100

    Add files via upload

---
+++ first version(keil)/msg_drive.h
@@ -0,0 +1,8 @@
+#include "uart_drive.h"
+#include "help_func.h"
+
+
+
+void basic_msg(char uart, int val);
+void make_msg(char uart,  char channels, int * data);
+void InsertData(int num,char * wip);
